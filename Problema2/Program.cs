7a639bf99d648044fb37e29dde3ca0de6ac89911
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-01 23:15:41 +0300

    rezolvare problema 2

---
+++ Problema2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Linq;
+
+namespace Problema_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = ReadNumberFromKeyboard("Va rog sa introduceti primul numar: ");
+            int secondnumber = ReadNumberFromKeyboard("Va rog sa introduceti al doilea numar: ");
+
+            int rezultatintreg = firstnumber / secondnumber;
+            int rest = firstnumber % secondnumber;
+
+            if (rest == 0)
+            {
+                Console.WriteLine("Impartirea poate fi facuta exact. Rezultatul intreg este: " + rezultatintreg);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu poate fi facuta exact. Rezultatul intreg este: " + rezultatintreg + " iar restul este: " + rest);
+            }
+
+            //nu vedeam rezultatul fara asta
+            Console.ReadLine();
+        }
+
+        static int ReadNumberFromKeyboard(string label)
+        {
+            bool canConvert = false;
+            while (!canConvert)
+            {
+                Console.Write(label);
+                string textRead = Console.ReadLine();
+
+                canConvert = int.TryParse(textRead, out int result);
+                if (canConvert)
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Textul " + textRead + " nu reprezinta un numar.");
+                }
+            }
+
+            return 0;
+        }
+    }
+}
